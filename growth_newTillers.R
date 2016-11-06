d7d41d553499f3dba54ac286035a031423111453
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-11-05 19:32:47 -0500

    New tillers models and graphs for all plots, and one sex plots only

--- growth_newTillers.R
+++ growth_newTillers.R
@@ -5,17 +5,16 @@ library(glmmADMB) # Fit models with a Negative Binomial
 library(dplyr)
 source("C:/Users/ac79/Documents/CODE/LLELA/analysis/model_avg.R")
 
-#read in data
+# load and format data -----------------------------------------------------
 d=read.csv("Data/vr.csv")
 #remove dead individuals (this is a GROWTH model!)
 d=subset(d, surv_t1 != 0)
 #logtransform leaf numbers
 d$plot=as.factor(d$plot) #glmmadmb wants plot as a factor
 
-
 # Year two
 tmp15=subset(d,year==2015)
-#remove 
+# remove 
 tmp15$new_t1[tmp15$new_t1=="SKIPPED"]=NA
 tmp15$new_t1[tmp15$new_t1=="cnf"]=NA
 tmp15$new_t1[tmp15$new_t1==""]=NA
@@ -26,10 +25,29 @@ d15=na.omit(tmp15[,c("l_t1","log_l_t0","plot","sex","new_t1",
 d15$new_t1_pc <- d15$new_t1 / d15$TotDensity
 d15 = subset(d15, plot != 149) # Remove outlier
 
-#Response variable: total number of tillers per capita -------------------------------------------
+# One sex plots -----------------------------------------------------------
+#remove 
+tmp = d
+tmp$new_t1[tmp$new_t1=="SKIPPED"]=NA
+tmp$new_t1[tmp$new_t1=="cnf"]=NA
+tmp$new_t1[tmp$new_t1==""]=NA
+tmp$new_t1=as.numeric(as.character(tmp$new_t1))
+d2=na.omit(tmp[,c("l_t1","log_l_t0","plot","sex","new_t1","F","M",
+                  "TotDensity","sr","year")])
+d2$oneSex             <- 0
+d2$oneSex[d2$F==0 | 
+            d2$M==0]  <- 1 # Flag treatments with only one sex 
+one_sex               <- subset(d2,oneSex == 1)
+# flag what is male, and what is female?
+one_sex$plot_sex      <- "m"
+one_sex$plot_sex[
+  one_sex$M==0]       <- "f"
+one_sex$plot_sex      <- factor(one_sex$plot_sex, levels = c("f", "m") )
 
-#INITIAL STRUCTURES
-#Effect of plot treatment on new tillers#################################
+
+# Model selection ##############################################################
+# PER CAPITA total number of tillers -------------------------------------------
+
 nt=list()
 # Effect of density
 nt[[1]] <- lm(new_t1_pc ~ TotDensity, data=d15)
@@ -49,39 +67,8 @@ grow_new_tpc_avg  <- model_avg(grow_new_tpc, nt)
 write.csv(grow_new_tpc_avg, "Results/VitalRates_3/growth_new_tpc_best.csv", row.names = F)
 
 
-#Graph: total number of tillers -----------------------------------------------------
-tiff("Results/VitalRates_3/growth_newTillers_pc.tiff",unit="in",width=6.3,height=3.15,res=600,compression="lzw")
+# Total number of tillers ------------------------------------------------
 
-par(mfcol=c(1,2),mar=c(2.5,2.5,0.1,0.1),mgp=c(1.4,0.5,0),oma=c(0,0,0,0.1))
-
-plot(d15$TotDensity,d15$new_t1_pc,pch=16,ylab="Per capita new tillers (2015)",
-     xlab="Planting density")
-
-#Calculate lines
-beta  <-  grow_new_tpc_avg[,c("predictor","avg")]$avg
-xSeq  <-  seq(0,48,by=1)
-#yL    <-  beta[1] + beta[2]*xSeq + beta[3]*xSeq^2 + beta[4]*0.1 + 
-#          beta[5]*xSeq*0.1 + beta[6]*xSeq^2*0.1
-#yH    <-  beta[1] + beta[2]*xSeq + beta[3]*xSeq^2 + beta[4]*0.9 + 
-#          beta[5]*xSeq*0.9 + beta[6]*xSeq^2*0.9
-yL    <-  beta[1] + beta[2]*0.1 + beta[3]*xSeq*0.1 + beta[4]*xSeq + 
-          beta[5]*xSeq^2 + beta[6]*xSeq^2*0.1
-yH    <-  beta[1] + beta[2]*0.9 + beta[3]*xSeq*0.9 + beta[4]*xSeq + 
-          beta[5]*xSeq^2 + beta[6]*xSeq^2*0.9
-
-lines(xSeq,yL,col="red",lwd=2, lty = 2)
-lines(xSeq,yH,col="blue",lwd=2)
-
-legend(10, 19, c("10% female plots", "90% female plots"),
-       lty = c(1,2), lwd=2, col=c("red","blue"), bty = "n")
-
-#dev.off()
-
-
-#Response variable: total number of tillers ------------------------------------------------
-
-#INITIAL STRUCTURES
-#Effect of plot treatment on new tillers#################################
 nt=list()
 # Effect of density
 nt[[1]] <- lm(new_t1 ~ TotDensity, data=d15)
@@ -100,20 +87,66 @@ grow_new_t_avg  <- model_avg(grow_new_t, nt)
 write.csv(grow_new_t_avg, "Results/VitalRates_3/growth_new_t_best.csv", row.names = F)
 
 
+# One sex plots ---------------------------------------------------------------------
+
+nt=list()
+#Effect of density
+nt[[1]] <- lm(new_t1 ~ year, data=one_sex)
+nt[[2]] <- lm(new_t1 ~ TotDensity + year, data=one_sex)
+nt[[3]] <- lm(new_t1 ~ TotDensity + year + sex, data=one_sex)
+nt[[4]] <- lm(new_t1 ~ TotDensity + sex, data=one_sex)
+#Effect of density + sex ratio
+nt[[5]] <- glmmadmb(new_t1 ~ year+ (1 | plot), data=one_sex, family="nbinom2")
+nt[[6]] <- glmmadmb(new_t1 ~ TotDensity + year + (1 | plot), data=one_sex,family="nbinom2")
+nt[[7]] <- glmmadmb(new_t1 ~ TotDensity + year + sex + (1 | plot), data=one_sex,family="nbinom2")
+nt[[8]] <- glmmadmb(new_t1 ~ TotDensity + sex + (1 | plot), data=one_sex,family="nbinom2")
+AICtab(nt,weights=T)
+
+oneSex_sel      <- AICtab(nt,weights=T)
+oneSex_new_avg  <- model_avg(oneSex_sel, nt)
+write.csv(oneSex_new_avg, "Results/VitalRates_3/oneSex_new_avg.csv", row.names = F)
+
+
+# Graph -----------------------------------------------------------------------------
+
 #Graph: total number of tillers -----------------------------------------------------
+tiff("Results/VitalRates_3/growth_newTillers.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
+
+par(mfrow=c(2,2),mar=c(2.5,2.5,0.1,0.1),mgp=c(1.4,0.5,0),oma=c(0,0,0,0.1))
+
+# Per capita amount of new tillers
+plot(d15$TotDensity,d15$new_t1_pc,pch=16,ylab="Per capita new tillers (2015)",
+     xlab="Planting density")
+beta  <-  grow_new_tpc_avg[,c("predictor","avg")]$avg
+xSeq  <-  seq(0,48,by=1)
+yL    <-  beta[1] + beta[2]*0.1 + beta[3]*xSeq*0.1 + beta[4]*xSeq + 
+          beta[5]*xSeq^2 + beta[6]*xSeq^2*0.1
+yH    <-  beta[1] + beta[2]*0.9 + beta[3]*xSeq*0.9 + beta[4]*xSeq + 
+          beta[5]*xSeq^2 + beta[6]*xSeq^2*0.9
+lines(xSeq,yL,col="red",lwd=2, lty = 2)
+lines(xSeq,yH,col="blue",lwd=2)
+legend(10, 19, c("10% female plots", "90% female plots"),
+       lty = c(1,2), lwd=2, col=c("red","blue"), bty = "n")
+
+# total number of tillers
 plot(d15$TotDensity,d15$new_t1,pch=16,ylab="Number of new tillers (2015)",
      xlab="Planting density")
-
-#Calculate lines
 beta  <-  grow_new_t_avg[,c("predictor","avg")]$avg
 xSeq  <-  seq(0,48,by=1)
 yL    <-  beta[1] + beta[2]*0.1 + beta[3]*xSeq*0.1 + 
           beta[4]*xSeq^2*0.1 + beta[5]*xSeq + beta[6]*xSeq^2
 yH    <-  beta[1] + beta[2]*0.9 + beta[3]*xSeq*0.1 + 
           beta[4]*xSeq^2*0.9 + beta[5]*xSeq + beta[6]*xSeq^2
-  
 lines(xSeq,yL,col="red",lwd=2, lty = 2)
 lines(xSeq,yH,col="blue",lwd=2)
+
+# One tiller plots
+par(mar=c(2.5,2.5,0.1,0.1), mgp=c(1.4,0.5,0))
+boxplot(new_t1 ~ plot_sex + year, data = one_sex,  ylab = "Number of new tillers",
+        col = c("blue", "red"), cex.names=0.5, 
+        names = c("2014", "2014", "2015", "2015") )
+legend(0.5,225, c("female","male"), fill=c("blue","red"), bty = "n")
+text(0.55,180,"Data from one-sex plots only",pos=4)
 
 dev.off()
 
