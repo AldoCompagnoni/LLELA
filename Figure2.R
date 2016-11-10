17e84fd39e6a512d62779223b9ab321ffed2da9e
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-11-10 16:03:46 -0600

    Seed viability based on *planting density*

--- Figure2.R
+++ Figure2.R
@@ -12,7 +12,7 @@ d           <- read.csv("Data/vr.csv")
 # best models
 n_flow_beta <- read.csv("Results/VitalRates_3/n_flowers_best.csv")
 fec_beta    <- read.csv("Results/VitalRates_3/fecuntity_best.csv")
-germ_beta   <- read.csv("Results/VitalRates_3/germination_best.csv")
+germ_beta   <- read.csv("Results/VitalRates_3/germination_dens_best.csv")
 
 
 # FORMAT DATA ---------------------------------------------------------------------
@@ -26,7 +26,7 @@ f14         <- na.omit(d14[,c("plot","log_l_t0","flowN_t1","sex","sr","TotDensit
 # MODEL PREDICTIONS ---------------------------------------------------------------
 
 # flowers per individual (flowering model, Figure 1a) -----------------------------
-predict_flower <- expand.grid("(Intercept)" = 1, 
+predict_flower <- expand.grid("Intercept" = 1, 
                               log_l_t0 = mean(f14$log_l_t0),
                               TotDensity = seq(1,48,1),
                               sr = seq(0,1,0.1),
@@ -35,70 +35,56 @@ predict_flower$sexmXTotDensity  <- predict_flower$sex * predict_flower$TotDensit
 predict_flower$log_l_t0Xsex     <- predict_flower$log_l_t0 * predict_flower$sex
 predict_flower$srXsexm          <- predict_flower$sr * predict_flower$sex
 # change order of predictors to match prediction matrix
-flow_beta = n_flow_beta[c(1,2,3,5,6,4,7,8),c("predictor","avg")]
-# Predictions
-predict_flower$pred_n_flow      <- exp( as.matrix(predict_flower) %*% flow_beta$avg )
+flow_beta     <- n_flow_beta[c(1,2,3,5,6,4,7,8),c("predictor","avg")]
+# Per capita number of flowers
+predict_flower$pc_flowers      <- exp( as.matrix(predict_flower) %*% flow_beta$avg )
 
 
-# flowers per plot 
-# Number of female flowers per plot
+# Number of female flowers per plot -----------------------------------------------
 females                   <- subset(predict_flower, sex == 0)
 females                   <- females[order(females$TotDensity,females$sr),]
 females$n_fem             <- females$sr * females$TotDensity
-females$n_flowers_f       <- females$n_fem * females$pred_n_flow
-
-# Number of male flowers per plot
-males                     <- subset(predict_flower, sex == 1)
-males                     <- males[order(males$TotDensity,males$sr),]
-males$n_mal               <- (1-males$sr) * males$TotDensity
-males$n_flowers_m         <- males$n_mal * males$pred_n_flow
-
-# plot level number of flowers and flower sex ratio
-n_plot_flowers            <- merge(select(females,sr,TotDensity,n_flowers_f),
-                                   select(males,  sr,TotDensity,n_flowers_m))
-n_plot_flowers$totFlowers <- n_plot_flowers$n_flowers_f + n_plot_flowers$n_flowers_m
-n_plot_flowers$sr_flowers <- n_plot_flowers$n_flowers_f / n_plot_flowers$totFlowers
+females$n_f_flowers       <- females$n_fem * females$pc_flowers
 
 
-# seeds per flower (fecundity model, Figure 1b) -----------------------------------
-predict_fec <- expand.grid("(Intercept)" = 1, 
+# Seeds per flower (fecundity model, Figure 1b) -----------------------------------
+predict_fec <- expand.grid("Intercept" = 1, 
                            log_l_t0 = mean( f14$log_l_t0 ),
                            sr = seq(0,1,0.1),
-                           TotDensity = seq(1,48,1))
-predict_fec$TotDensityXsr         <- predict_fec$TotDensity * predict_fec$sr
-predict_fec$pred_percapita_seeds  <- exp( as.matrix(predict_fec) %*% fec_beta$avg )
+                           TotDensity = seq(1,48,1) )
+predict_fec$TotDensityXsr <- predict_fec$TotDensity * predict_fec$sr
+predict_fec$per_f_seeds   <- exp( as.matrix(predict_fec) %*% fec_beta$avg )
 
 
-# seed viability (viability model, Figure 1c) -------------------------------------
-predict_viab              <- n_plot_flowers
-predict_viab$intersect    <- 1
-predict_viab$srXtotFlow   <- predict_viab$totFlowers * predict_viab$sr_flowers
-pred_viab_mat             <- select(predict_viab,intersect,sr_flowers,totFlowers,srXtotFlow)
-predict_viab$pred_viab    <- inv.logit( as.matrix(pred_viab_mat) %*% germ_beta$avg)
+# Seed viability (viability model, Figure 1c) -------------------------------------
+predict_viab              <- select(predict_fec,Intercept,sr,TotDensity,TotDensityXsr)
+predict_viab$pred_viab    <- inv.logit( as.matrix(predict_viab) %*% germ_beta$avg)
 
+
+# Seeds per plot ----------------------------------------------------------------
+seed_x_plot               <- merge(select(females,    sr, TotDensity, n_f_flowers),
+                                   select(predict_fec,sr, TotDensity, per_f_seeds))
+seed_x_plot$n_seeds       <-  seed_x_plot$n_f_flowers * seed_x_plot$per_f_seeds
 
 # Final data frame ----------------------------------------------------------------
-fert                 <- merge(select(predict_viab,sr,TotDensity,n_flowers_f,pred_viab),
-                              select(predict_fec,sr,TotDensity,pred_percapita_seeds))
-# n of flowers X seeds per capita X viability per seed
-fert$fertility       <- fert$n_flowers_f * fert$pred_percapita_seeds * fert$pred_viab 
+fert                 <- merge(select(seed_x_plot, sr,TotDensity,n_seeds),
+                              select(predict_viab,sr,TotDensity,pred_viab))
+fert$viable_seeds    <- fert$pred_viab * fert$n_seeds
 fert                 <- fert[order(fert$TotDensity,fert$sr),]
-fert$pc_fert         <-  fert$fertility / fert$n_flowers_f # per capita fertility
-fert$pc_fert[is.nan(fert$pc_fert)] = NA
 
 # FIGURE 2 ------------------------------------------------------------------------
 
 # Prepare data
 x<-unique(fert$sr)
 y<-unique(fert$TotDensity)
-z<-matrix(fert$fertility, nrow=length(unique(fert$sr)),
+z<-matrix(fert$viable_seeds, nrow=length(unique(fert$sr)),
           ncol=length(unique(females$TotDensity)))
 #z<-matrix(fert$ciao, nrow=length(unique(fert$sr)),
 #          ncol=length(unique(females$TotDensity)))
 
 
 
-tiff("Results/VitalRates_3/figure2.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
+tiff("Results/VitalRates_3/figure2_a_tetra.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
 
 persp(x,y,z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
       xlab = "Proportion of female individuals", 
@@ -109,7 +95,7 @@ persp(x,y,z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
 dev.off()
 
 # Alternatives to persp() function
-tiff("Results/VitalRates_3/figure2_contour.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
+tiff("Results/VitalRates_3/figure2_a_tetra_contour.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
 
 par(mar=c(4,4,1,0.5),mgp=c(2,0.7,0))
 filled.contour(x,y,z, color.palette=heat.colors, cex.lab = 1.4,
