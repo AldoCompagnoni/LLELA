42879d1eb421fb280eae155d9029d1d65b58db7f
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-11-10 15:20:54 -0600

    Streamlined lines to format data

--- growth_sexRatio.R
+++ growth_sexRatio.R
@@ -22,7 +22,14 @@ tmp15$new_t1[tmp15$new_t1=="SKIPPED"]=NA
 tmp15$new_t1[tmp15$new_t1=="cnf"]=NA
 tmp15$new_t1[tmp15$new_t1==""]=NA
 tmp15$new_t1=as.numeric(as.character(tmp15$new_t1))
-d15=na.omit(tmp15[,c("l_t1","log_l_t0","plot","sex","new_t1","sr","TotDensity")])
+d15=na.omit(tmp15[,c("l_t1","log_l_t0","plot","focalI","sex","new_t1","sr","TotDensity")])
+
+#Merge
+tmp     <- select(d14, plot, new_t1)
+tmp     <- mutate(tmp,new_t1 = as.character(as.numeric(new_t1)))   
+tmp     <- mutate(tmp, new_t0 = new_t1, new_t1 = NULL)
+d14_15  <- merge(tmp, d15)
+
 
 # 2014 --------------------------------------------------------------------------------
 
@@ -79,6 +86,7 @@ m14L[[15]]=glmmadmb(l_t1 ~ log_l_t0 * sex + sr + c_t0 + sr:sex + c_t0:sex  + (1 
 gr14_L_mod_sel  <- AICtab(m14L, weights = T)
 gr14_L_avg      <- model_avg(gr14_L_mod_sel, m14L)
 write.csv(gr14_L_avg, "Results/VitalRates_3/growth_14L_best.csv", row.names = F)
+
 
 # 2015 --------------------------------------------------------------------------------
 
@@ -138,7 +146,6 @@ grow_avgN   <- model_avg(grow_select, lMod)
 write.csv(grow_avgN, "Results/VitalRates_3/growthN_best.csv", row.names = F)
 
 
-
 # GRAPH ------------------------------------------------------------------------------------------------
 
 # Best models
@@ -152,21 +159,19 @@ grow_avgN  <- read.csv("Results/VitalRates_3/growthN_best.csv")
 # 2015
 d15$col=as.integer(d15$sex)
 d15$col=as.character(factor(d15$col,labels=c("blue","red")))
-d15$symb=as.integer(as.character(factor(d15$col,labels=c("17","16"))))
 # 2014
 d14$col=as.integer(d14$sex)
 d14$col=as.character(factor(d14$col,labels=c("blue","red")))
-d14$symb=as.integer(as.character(factor(d14$col,labels=c("17","16"))))
 
 # 2015 ---------------------------------------------------------------------------------------
 
 tiff("Results/VitalRates_3/growth_raw.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
 
-par(mfrow=c(2,2),mar=c(2.5,2.5,0.1,0.1),mgp=c(1.4,0.5,0),oma=c(0,0,0,0))
+par(mfrow=c(2,2),mar=c(3,2.5,1,0.1),mgp=c(1.4,0.5,0),oma=c(0,0,0,0))
 
 # 2015 --------------------------------------------------------------------------
-plot(d15$new_t1,d15$l_t1, pch=16, ylab="Number of target leaves (2015)",
-     xlab="Total number of new tillers",col=d15$col, ylim = c(0,82))
+plot(d15$new_t1,d15$l_t1, pch=16, ylab="Target individuals: number  leaves in 2015",
+     xlab="Total number of new tillers in plot (2015)",col=d15$col, ylim = c(0,99))
 beta <- grow_avg[,c("predictor","avg")]$avg
 xSeq <- seq(0,224,by=1 )
 size <- mean( d15$log_l_t0 )
@@ -176,12 +181,13 @@ y_f <- exp( beta[1] + beta[2]*size + beta[3]*xSeq + beta[5]*0.5 )
 lines(xSeq,y_f,lwd=1.5,lty=1,col="blue")
 lines(xSeq,y_m,lwd=1.5,lty=1,col="red")
 
-legend(-10,88,c("Males","Females"),
+legend(-10,99,c("Males","Females"),
        lty=1,lwd=1.5,col=c("red","blue"),bty="n")
+title("2015")
 
 # Density
-plot(d15$TotDensity,d15$l_t1, pch=16, ylab="Number of target leaves (2015)",
-     xlab="Planting density",col=d15$col, ylim = c(0,82))
+plot(d15$TotDensity,d15$l_t1, pch=16, ylab="Target individuals: number  leaves in 2015",
+     xlab="Planting density",col=d15$col, ylim = c(0,99))
 beta <- grow_avgN[,c("predictor","avg")]$avg
 xSeq <- seq(0,48,by=1)
 size <- mean(d15$log_l_t0)
@@ -190,13 +196,14 @@ y_m <- exp( beta[1] + beta[2]*size + beta[3] + beta[4]*0.5 +
 y_f <- exp( beta[1] + beta[2]*size + beta[4]*0.5 + beta[5]*xSeq )
 lines(xSeq,y_f,lwd=1.5,lty=1,col="blue")
 lines(xSeq,y_m,lwd=1.5,lty=1,col="red")
+title("2015")
 
 
 # 2014 -------------------------------------------------------------------------
 
 # Total number of leaves
-plot(d14$c_t0, d14$l_t1, pch=16, ylab="Number of target leaves (2014)",
-     xlab="Total number of plot leaves",col=d14$col, ylim = c(0,82))
+plot(d14$c_t0, d14$l_t1, pch=16, ylab="Target individuals: number  leaves in 2014",
+     xlab="Total number of plot leaves in fall 2013",col=d14$col, ylim = c(0,99))
 beta = gr14_L_avg[,c("predictor","avg")]$avg
 xSeq <- seq(0,max(d14$c_t0),by=1)
 sr   <- 0.5
@@ -206,10 +213,11 @@ y_m <- exp( beta[1] + beta[2]*xSeq + beta[3]*size + beta[4] +
 y_f <- exp( beta[1] + beta[2]*xSeq + beta[3]*size + beta[6]*0.5)
 lines(xSeq,y_f,lwd=1.5,lty=1,col="blue")
 lines(xSeq,y_m,lwd=1.5,lty=1,col="red")
+title("2014")
 
 # Planting density
-plot(d14$TotDensity, d14$l_t1, pch=16, ylab="Number of target leaves (2014)",
-     xlab="Planting density",col=d14$col, ylim = c(0,82))
+plot(d14$TotDensity, d14$l_t1, pch=16, ylab="Target individuals: number  leaves in 2014",
+     xlab="Planting density",col=d14$col, ylim = c(0,99))
 beta <- gr14_avg[,c("predictor","avg")]$avg
 xSeq <- seq(0,48,by=1)
 sr   <- 0.5
@@ -219,5 +227,6 @@ y_m <- exp( beta[1] + beta[2]*size + beta[3] + beta[4]*xSeq +
 y_f <- exp( beta[1] + beta[2]*size + beta[4]*xSeq + beta[6]*sr )
 lines(xSeq,y_f,lwd=1.5,lty=1,col="blue")
 lines(xSeq,y_m,lwd=1.5,lty=1,col="red")
+title("2014")
 
 dev.off()
