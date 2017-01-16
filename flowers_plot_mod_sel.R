6d3b8537e2ee04ee71d831a93acdbfea749b0121
AldoCompagnoni <aldo.compagnoni@gmail.com>
2017-01-16 14:54:00 -0600

    Plot panicule num. as function of plot planting characteristics.

---
+++ flowers_plot_mod_sel.R
@@ -0,0 +1,83 @@
+##Sex predictor of flowering probability: SEX RATIO (female individuals/total individuals)
+setwd("C:/Users/ac79/Downloads/Dropbox/POAR--Aldo&Tom/Response-Surface experiment/Experiment/Implementation")
+library(bbmle)
+library(boot)
+library(glmmADMB)
+source("C:/Users/ac79/Documents/CODE/LLELA/analysis/model_avg.R")
+
+
+# Data --------------------------------------------------------------------------------
+pans      <- read.csv("Data/Spring 2014/plot_level_panicles.csv")
+pans_f    <- subset(pans, F != 0) # plots containing females
+pans_m    <- subset(pans, M != 0) # plots containing males
+
+
+# Model selection ---------------------------------------------------------------------
+
+
+# females -----------------------------------------------------------------------------
+fMod        <- list()
+# poisson models
+fMod[[1]]   <- glm(F_flow_t1 ~ 1,      data=pans_f,family="poisson")
+fMod[[2]]   <- glm(F_flow_t1 ~ sr,     data=pans_f,family="poisson")
+fMod[[3]]   <- glm(F_flow_t1 ~ N,      data=pans_f,family="poisson")
+fMod[[4]]   <- glm(F_flow_t1 ~ N + sr, data=pans_f,family="poisson")
+fMod[[5]]   <- glm(F_flow_t1 ~ N * sr, data=pans_f,family="poisson")
+# negative binomial models
+fMod[[6]]   <- glm.nb(F_flow_t1 ~ 1,     data=pans_f)
+fMod[[7]]   <- glm.nb(F_flow_t1 ~ sr,    data=pans_f)
+fMod[[8]]   <- glm.nb(F_flow_t1 ~ N,     data=pans_f)
+fMod[[9]]   <- glm.nb(F_flow_t1 ~ N + sr,data=pans_f)
+fMod[[10]]  <- glm.nb(F_flow_t1 ~ N * sr,data=pans_f)
+
+# Model average
+f_pan_sel   <- AICtab(fMod,weights=T)
+f_pan_avg   <- model_avg(f_pan_sel, fMod)
+write.csv(flow_avg, "Results/VitalRates_3/f_flowers_plot_best.csv", row.names = F)
+
+
+# males -------------------------------------------------------------------------------
+mMod        <- list()
+# poisson models
+mMod[[1]]   <- glm(M_flow_t1 ~ 1,      data=pans_m,family="poisson")
+mMod[[2]]   <- glm(M_flow_t1 ~ sr,     data=pans_m,family="poisson")
+mMod[[3]]   <- glm(M_flow_t1 ~ N,      data=pans_m,family="poisson")
+mMod[[4]]   <- glm(M_flow_t1 ~ N + sr, data=pans_m,family="poisson")
+mMod[[5]]   <- glm(M_flow_t1 ~ N * sr, data=pans_m,family="poisson")
+# negative binomial models
+mMod[[6]]   <- glm.nb(M_flow_t1 ~ 1,     data=pans_m)
+mMod[[7]]   <- glm.nb(M_flow_t1 ~ sr,    data=pans_m)
+mMod[[8]]   <- glm.nb(M_flow_t1 ~ N,     data=pans_m)
+mMod[[9]]   <- glm.nb(M_flow_t1 ~ N + sr,data=pans_m)
+mMod[[10]]  <- glm.nb(M_flow_t1 ~ N * sr,data=pans_m)
+
+# Model average
+m_pan_sel   <- AICtab(mMod,weights=T)
+m_pan_avg   <- model_avg(m_pan_sel, mMod)
+write.csv(m_pan_avg, "Results/VitalRates_3/m_flowers_plot_best.csv", row.names = F)
+
+
+# Graph -----------------------------------------------------------------------------
+
+# Plot data
+plot(jitter(pans_f$F_flow_t1) ~ pans_f$N, xlab=expression("Planting density"),
+     ylab="Number of flowers", pch=16, col="blue", ylim = c(-0.5,90))
+par(new=T) ; 
+plot(jitter(pans_m$M_flow_t1) ~ pans_m$N,pch=17,xlab="",ylab="",col="red",
+     xaxt="n", ylim = c(-0.5,90))
+
+# lines
+xSeq  <- seq(1,48,1)
+betaF <- f_pan_avg$avg
+betaM <- m_pan_avg$avg
+
+yF_h  <- exp(betaF[1] + betaF[2]*xSeq + betaF[3]*xSeq*0.9 + betaF[4]*0.9)
+yF_l  <- exp(betaF[1] + betaF[2]*xSeq + betaF[3]*xSeq*0.2 + betaF[4]*0.2)
+yM_h  <- exp(betaM[1] + betaM[2]*xSeq + betaM[3]*xSeq*0.9 + betaM[4]*0.9)
+yM_l  <- exp(betaM[1] + betaM[2]*xSeq + betaM[3]*xSeq*0.2 + betaM[4]*0.2)
+
+lines(xSeq, yF_h, col = "blue", lwd = 2, lty = 1)
+lines(xSeq, yF_l, col = "blue", lwd = 2, lty = 2)
+lines(xSeq, yM_h, col = "red",  lwd = 2, lty = 1)
+lines(xSeq, yM_l, col = "red",  lwd = 2, lty = 2)
+
