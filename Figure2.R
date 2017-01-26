1597af6f52927684f65697b679364728ec4e886c
AldoCompagnoni <aldo.compagnoni@gmail.com>
2017-01-25 19:30:16 -0600

    Fig2 for 'per_capita', 'per_female', and 'absolute' results

--- Figure2.R
+++ Figure2.R
@@ -19,7 +19,7 @@ newt_beta   <- read.csv("Results/VitalRates_3/new_t_best.csv", stringsAsFactors 
 
 # Flowers per individual (flowering model, Figure 1a) -----------------------------
 des_flower    <- new_design(n_flow_beta)
-pred_flower   <- pred(des_flower, n_flow_beta, pc_n_flowers, exp)
+pred_flower   <- pred(des_flower, n_flow_beta, n_flowers_pc, exp)
 
 # Seeds per flower (panicle) Figure 1b) 
 des_seed      <- new_design(fec_beta)
@@ -34,15 +34,18 @@ pred_viab     <- pred(des_viab, germ_beta, pred_viab, inv.logit)
 # new tillers
 des_till      <- expand.grid(TotDensity = seq(1,48,1), sr = seq(0,1,0.05) )
 pred_till     <- des_till %>% mutate(pred_new_t = (newt_beta[,1]*TotDensity)/(1+newt_beta[,2]*TotDensity))
-
-
+pred_till     <- mutate(pred_till, pred_new_t_pc = pred_new_t/TotDensity)
+pred_till     <- mutate(pred_till, pred_new_t_pf = pred_new_t/(TotDensity*sr) )
+pred_till     <- mutate(pred_till, pred_new_t_pf = replace(pred_new_t_pf, pred_new_t_pf==Inf, NA))
+                        
 # COMBINE MODELS #####################################################################
 
 # PANEL 1: Per capita/total seed production ------------------------------------------------
-seed_prod <- merge(dplyr::select(subset(pred_flower, sexm == 0), sr,TotDensity,pc_n_flowers), 
-                   dplyr::select(pred_seed, sr,TotDensity,seeds_per_f) )
-seed_prod <- mutate(seed_prod, pc_seed_prod = seeds_per_f * pc_n_flowers)
-seed_prod <- mutate(seed_prod, seed_prod    = pc_seed_prod * sr * TotDensity)
+seed_prod <- merge(select(subset(pred_flower, sexm == 0), sr,TotDensity,n_flowers_pc), 
+                   select(pred_seed, sr,TotDensity,seeds_per_f) )
+seed_prod <- mutate(seed_prod, seed_prod    = seeds_per_f * n_flowers_pc * sr * TotDensity)
+seed_prod <- mutate(seed_prod, seed_prod_pf = seed_prod / (sr * TotDensity))
+seed_prod <- mutate(seed_prod, seed_prod_pc = seed_prod / TotDensity)
 
 # PANEL 3: Viable Seed production ----------------------------------------------------
 
@@ -50,11 +53,11 @@ seed_prod <- mutate(seed_prod, seed_prod    = pc_seed_prod * sr * TotDensity)
 # Number of female flowers (panicles) per plot 
 females       <- subset(pred_flower, sexm == 0)
 females       <- mutate(females, n_fem       = sr * TotDensity) # n. of female flowers in plot
-females       <- mutate(females, n_f_flowers = n_fem * pc_n_flowers) #n. of fem. panicles
+females       <- mutate(females, n_f_flowers = n_fem * n_flowers_pc) #n. of fem. panicles
 # Number of male flowers (panicles) per plot 
 males         <- subset(pred_flower, sexm == 1)
 males         <- mutate(males, n_mal       = (1-sr) * TotDensity) # n. of female flowers in plot
-males         <- mutate(males, n_m_flowers = n_mal * pc_n_flowers) #n. of fem. panicles
+males         <- mutate(males, n_m_flowers = n_mal * n_flowers_pc) #n. of fem. panicles
 # combine the data sets
 flow_num      <- merge(dplyr::select(females,TotDensity,sr,n_fem,n_f_flowers), 
                        dplyr::select(males,  TotDensity,sr,n_mal,n_m_flowers))
@@ -80,35 +83,110 @@ names(des_v_s)[c(7,10)] = c("(Intercept)","sr_f:totFlow")
 pred_viab_s   <- merge(des_v_s,pred_v_seed)
 fert          <- merge(pred_viab_s, seed_prod)
 fert          <- mutate(fert, viable_seeds     = seed_prod * viab,
-                              viable_seeds_pc  = (seed_prod * viab)/n_fem)
+                              viable_seeds_pf  = (seed_prod * viab)/n_fem)
 
 # reproduction
 reprod    <- merge(fert,pred_till)
-reprod    <- mutate(reprod, reprod = viable_seeds + pred_new_t)
+reprod    <- mutate(reprod, reprod    = viable_seeds + pred_new_t)
+reprod    <- mutate(reprod, reprod_pf = (viable_seeds + pred_new_t)/n_fem)
+reprod    <- mutate(reprod, reprod_pc = (viable_seeds + pred_new_t)/TotDensity)
 
 # FIGURE 2 ------------------------------------------------------------------------------
 
-# data for 3d graph
-seed_3d <- form_3d_surf(seed_prod,seed_prod)
+# data for PER FEMALE data
+seed_3d <- form_3d_surf(seed_prod,seed_prod_pf)
 viab_3d <- form_3d_surf(pred_viab,pred_viab)
-till_3d <- form_3d_surf(pred_till,pred_new_t)
-vs_3d   <- form_3d_surf(fert,viable_seeds)
+till_3d <- form_3d_surf(pred_till,pred_new_t_pf)
+vs_3d   <- form_3d_surf(reprod,reprod_pf)
 
-tiff("Results/VitalRates_3/figure2_abs.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
+tiff("Results/VitalRates_3/figure2_pf.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
 
 m_line <- -2
-par(mfrow=c(2,2),mar=c(0,2,0,0),mgp=c(3,1,0), oma=c(0,0,0,0),cex=0.8,
+par(mfrow=c(2,2),mar=c(0,2,0,0),mgp=c(3,1,0), oma=c(0,0,0,0),cex=0.7,
     cex.axis = 0.7)
 
 persp(seed_3d$x,seed_3d$y,seed_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
       xlab = "Proportion of female individuals",line = m_line,
       ylab = "Density",ticktype = "detailed",
-      zlab = "Seeds per individual",mgp=c(2,1,0),
-      main = "Plot-level seed productions")
+      zlab = "Seed produced_PF",mgp=c(2,1,0),
+      main = "Seed production_PF")
 persp(viab_3d$x,viab_3d$y,viab_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
       xlab = "Proportion of female individuals", line = m_line,
       ylab = "Density",ticktype = "detailed",
-      zlab = "Proportion viable",
+      zlab = "Seed viability",
+      main = "Seed viability")
+persp(till_3d$x,till_3d$y,till_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals", line = m_line,
+      ylab = "Density",ticktype = "detailed",
+      zlab = "New Tillers_PF",
+      main = "Production of new tillers_PF")
+persp(vs_3d$x,vs_3d$y,vs_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals", 
+      ylab = "Density",ticktype = "detailed",line = m_line,
+      zlab = "Viable recruits_PF",
+      main = "Reproduction_PF")
+
+dev.off()
+
+
+# data for PER CAPITA data
+seed_3d <- form_3d_surf(seed_prod,seed_prod_pc)
+viab_3d <- form_3d_surf(pred_viab,pred_viab)
+till_3d <- form_3d_surf(pred_till,pred_new_t_pc)
+vs_3d   <- form_3d_surf(reprod,reprod_pc)
+
+tiff("Results/VitalRates_3/figure2_pc.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
+
+m_line <- -2
+par(mfrow=c(2,2),mar=c(0,2,0,0),mgp=c(3,1,0), oma=c(0,0,0,0),cex=0.7,
+    cex.axis = 0.7)
+
+persp(seed_3d$x,seed_3d$y,seed_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals",line = m_line,
+      ylab = "Density",ticktype = "detailed",
+      zlab = "Seed produced_PC",mgp=c(2,1,0),
+      main = "Seed production_PC")
+persp(viab_3d$x,viab_3d$y,viab_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals", line = m_line,
+      ylab = "Density",ticktype = "detailed",
+      zlab = "Seed viability",
+      main = "Seed viability")
+persp(till_3d$x,till_3d$y,till_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals", line = m_line,
+      ylab = "Density",ticktype = "detailed",
+      zlab = "New Tillers_PC",
+      main = "Production of new tillers_PC")
+persp(vs_3d$x,vs_3d$y,vs_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals", 
+      ylab = "Density",ticktype = "detailed",line = m_line,
+      zlab = "Viable recruits_PC",
+      main = "Reproduction_PC")
+
+dev.off()
+
+
+
+# data for ABSOLUTE data
+seed_3d <- form_3d_surf(seed_prod,seed_prod)
+viab_3d <- form_3d_surf(pred_viab,pred_viab)
+till_3d <- form_3d_surf(pred_till,pred_new_t)
+vs_3d   <- form_3d_surf(reprod,reprod)
+
+tiff("Results/VitalRates_3/figure2_abs.tiff",unit="in",width=6.3,height=6.3,res=600,compression="lzw")
+
+m_line <- -2
+par(mfrow=c(2,2),mar=c(0,2,0,0),mgp=c(3,1,0), oma=c(0,0,0,0),cex=0.7,
+    cex.axis = 0.7)
+
+persp(seed_3d$x,seed_3d$y,seed_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals",line = m_line,
+      ylab = "Density",ticktype = "detailed",
+      zlab = "Seed produced",mgp=c(2,1,0),
+      main = "Plot-level seed production")
+persp(viab_3d$x,viab_3d$y,viab_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
+      xlab = "Proportion of female individuals", line = m_line,
+      ylab = "Density",ticktype = "detailed",
+      zlab = "Seed viability",
       main = "Seed viability")
 persp(till_3d$x,till_3d$y,till_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
       xlab = "Proportion of female individuals", line = m_line,
@@ -118,7 +196,15 @@ persp(till_3d$x,till_3d$y,till_3d$z, theta = 30, phi = 30, expand = 0.5, col = "
 persp(vs_3d$x,vs_3d$y,vs_3d$z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
       xlab = "Proportion of female individuals", 
       ylab = "Density",ticktype = "detailed",line = m_line,
-      zlab = "Viable seeds",
+      zlab = "Viable recruits",
       main = "Reproduction")
 
 dev.off()
+
+
+
+par(mar=c(4,4,1,0.5),mgp=c(2,0.7,0))
+filled.contour(vs_3d$x,vs_3d$y,vs_3d$z, color.palette=heat.colors, cex.lab = 1.4,
+               xlab = "Proportion of female individuals", 
+               ylab = "Planting density", 
+               main = "Number of viable seeds")
