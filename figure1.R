5a05b436e6741bdfc75ff9dd0b9250bdee2db615
AldoCompagnoni <aldo.compagnoni@gmail.com>
2017-03-01 20:31:09 -0600

    More dplyr grammar.

--- figure1.R
+++ figure1.R
@@ -4,9 +4,11 @@ library(bbmle)
 library(dplyr)
 library(boot)
 source("C:/Users/ac79/Documents/CODE/LLELA/model_avg.R")
+source("C:/Users/ac79/Documents/CODE/LLELA/unload_mass.R")
+unload_mass()
 
 #Read data--------------------------------------------------------------------
-d           <- read.csv("Data/vr.csv")
+d           <- read.csv("Data/vr.csv", stringsAsFactors = F)
 fem_seeds   <- read.csv("Data/Spring 2014/SeedCount/Poa Arachnifera_seedCount.csv")
 m_spiklet   <- read.csv("Data/Spring 2014/maleCounts/malePaniculesSpring2014.csv")
 viabVr      <- read.csv("Data/Spring 2014/viability/tetra_germ_plot_data.csv",
@@ -23,16 +25,19 @@ germ_beta   <- read.csv("Results/VitalRates_3/germination_best.csv")
 
 # Number of flowers in 2014
 f14   <- format_flower(d)
+f14   <- f14 %>% mutate( plot = as.numeric(as.character(plot)) )
 
 # fecundity data  
 fem_seeds   <- mutate(fem_seeds, focalI = paste("f",IndividualN,sep=""))
 fem_seeds   <- dplyr::select(fem_seeds,Plot,focalI,SeedN)
-fem_seeds   <- filter(fem_seeds, !is.na(SeedN) )
-fem_seeds   <- rename(fem_seeds, plot = Plot)
-fecund_data <- merge(d14,fem_seeds)
+fem_seeds   <- subset(fem_seeds, !is.na(SeedN) )
+fem_seeds   <- mutate(fem_seeds, plot = Plot)
+fecund_data <- merge(fem_seeds, select(f14,plot,sr, TotDensity))
+fecund_data <- fecund_data %>% unique()
 
 # male allocation (spikelets) data  
-m_alloc     <- merge(d14, m_spiklet)
+m_alloc     <- merge(select(f14,plot,sr,TotDensity), m_spiklet)
+m_alloc     <- m_alloc %>% unique()
 
 # remove three plots with more than 60 flowers 
 viabVr      <- subset(viabVr, totFlow < 60)
@@ -61,10 +66,12 @@ mal14   <- subset(f14,sex=="m")
 fem14   <- subset(f14,sex=="f")
 
 # means
-meanF   <- fem14 %>% group_by(TotDensity) %>% 
-  summarise( meanF = mean(flowN_t1), sdF = sd(flowN_t1) )
-meanM   <- mal14 %>% group_by(TotDensity) %>% 
-  summarise( meanM = mean(flowN_t1), sdM = sd(flowN_t1) )
+meanF   <- fem14 %>% 
+            group_by(TotDensity) %>% 
+            summarise( meanF = mean(flowN_t1), sdF = sd(flowN_t1) )
+meanM   <- mal14 %>% 
+            group_by(TotDensity) %>% 
+            summarise( meanM = mean(flowN_t1), sdM = sd(flowN_t1) )
 
 # plots
 plot(meanF$TotDensity , meanF$meanF, pch = 16, col = "blue", ylim = c(0.3,2),
@@ -82,8 +89,8 @@ beta  <- n_flow_beta[,c("predictor","avg")]$avg
 y_m <- exp(beta[1] + beta[2]*xSeq + beta[3] + beta[4]*0.5 + 
            beta[5]*0.5*xSeq + beta[6]*xSeq + beta[7]*0.5)
 y_f <- exp(beta[1] + beta[2]*xSeq + beta[4]*0.5 + beta[5]*0.5*xSeq)
-lines(xSeq,y_m,lty=1,lwd=2,col="blue")
-lines(xSeq,y_f,lty=2,lwd=2,col="red")
+lines(xSeq,y_m,lty=1,lwd=2,col="red")
+lines(xSeq,y_f,lty=2,lwd=2,col="blue")
 
 legend(1,2.1,c("male","female"), lty=1, lwd=2, 
        col=c("red","blue"), bty="n", pch = 16 )
@@ -111,10 +118,10 @@ text(par("usr")[1] - (par("usr")[2] - par("usr")[1])*0.11,
 
 colfunc = colorRampPalette(cRamp(unique(arrange(fecund_data,sr)$sr)))
 legend_image <- as.raster(matrix(colfunc(19), ncol=1))
-text(x=24.5, y = seq(850,1000,l=3), labels = seq(0,1,l=3))
+text(x=24.5, y = seq(850,1000,l=3), labels = seq(1,0,l=3))
 rasterImage(legend_image, 17, 850, 22, 1000)
 text(25, 1000, "Percent of", pos = 4)
-text(25, 925, "males in", pos = 4)
+text(25, 925, "females in", pos = 4)
 text(25, 850, "plot", pos = 4)
 
 
