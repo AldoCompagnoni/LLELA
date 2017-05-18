29497b37d21fbf1262a555fcda2aa55d659f4e97
AldoCompagnoni <aldo.compagnoni@gmail.com>
2017-05-18 10:48:39 -0500

    Add rgr_mod_sel function

--- model_sel_results.R
+++ model_sel_results.R
@@ -31,3 +31,32 @@ sel_results <- function(x, candidate_num, response){
   return(sel_res)
   
 }
+
+# Final model selection function - implemented with RGR on 4.26.2017
+rgr_mod_sel <- function(x){
+  
+  form_extract <- function(x){
+    
+    tmp <- formula(x) %>%
+      deparse() %>%
+      strsplit("~") 
+    # only left part of formula
+    tmp <- tmp[[1]][[2]]
+    tmp <- gsub("\\+ \\(1 \\| plot\\)", "", tmp)
+    tmp <- trimws(tmp)
+  }
+  
+  mod_structures  <- lapply(x, form_extract) %>%
+    unlist()
+  
+  # model ranks plus mode weights  
+  mod_sel_tab <- AICtab(x, weights = T, sort = F)
+  mod_rank    <- do.call(rbind, strsplit(attributes(mod_sel_tab)$row.names , "model"))
+  mod_rank    <- data.frame( Model = as.numeric(mod_rank[,2]),
+                             dAIC  = mod_sel_tab$dAIC,
+                             AIC_weight = mod_sel_tab$weight )
+  mod_rank    <- mutate(mod_rank, Equation = mod_structures)
+  
+  return(mod_rank)
+  
+}
