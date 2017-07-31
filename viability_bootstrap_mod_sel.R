c3ae4b8a4661c904c331acd36ad2bb94baf63847
Compagnoni <ac22qawo@idiv.de>
2017-07-31 16:51:29 +0200

    Bootstrap viability analyses, including max. 2 one-female-only plots at a time

--- viability_bootstrap_mod_sel.R
+++ viability_bootstrap_mod_sel.R
@@ -3,12 +3,14 @@
 # setwd("C:/Users/ac79/Downloads/Dropbox/POAR--Aldo&Tom/Response-Surface experiment/Experiment/Implementation")
 setwd("C:/cloud/Dropbox/POAR--Aldo&Tom/Response-Surface experiment/Experiment/Implementation")
 library(lme4) ; library(bbmle) ; library(boot) ; library(testthat)
-library(dplyr) ; library(tidyr)
-library(glmmADMB)
+library(dplyr) ; library(tidyr) ; library(gtools)
+library(glmmADMB) ; library(parallel)
 # source("C:/Users/ac79/Documents/CODE/LLELA/model_avg.R")
 # source("C:/Users/ac79/Documents/CODE/LLELA/model_sel_results.R")
 source("C:/CODE/LLELA/model_avg.R")
 source("C:/CODE/LLELA/model_sel_results.R")
+source("C:/CODE/LLELA/model_avg.R")
+source("C:/CODE/LLELA/prediction.R")
 
 
 # Read in data and format -----------------------------------------------------------
@@ -37,7 +39,8 @@ germ_dat  <- na.omit(dplyr::select(viabVr,germTot,germFail,sr_f,totFlow,sr,totN,
 one_f_2   <- combinations(8, 2, germ_dat$perm_id)
 one_f_3   <- combinations(8, 3, germ_dat$perm_id)
 one_f_4   <- combinations(8, 4, germ_dat$perm_id)
-   
+  
+detach("package:gtools")
 
 # # Viability model selection ---------------------------------------------------------
 # 
@@ -62,6 +65,20 @@ one_f_4   <- combinations(8, 4, germ_dat$perm_id)
 # AICtab(tSize, weights=T)
 
 
+# parallel processing set up ---------------------------------------------------------------
+
+# detect cores
+cores     <- detectCores()
+# set up as many clusters as detected by 'detectCores()'
+cluster   <- parallel::makePSOCKcluster(cores)
+
+# attach packages that will be needed on each cluster
+clusterEvalQ(cluster, list(library(lme4), library(glmmADMB), library(bbmle) , library(boot) , 
+                           library(testthat), library(dplyr) , library(tidyr)) )
+# attach objects that will be needed on each cluster
+clusterExport(cluster, "model_avg")
+
+
 # 1. Viable Seed Number: germination assays (germTot / germFail)---------------------
 
 # model structures
@@ -72,15 +89,16 @@ candidate_mods <- list(
   cbind(germTot,germFail) ~ sr_f + totFlow + (1 | plot),
   cbind(germTot,germFail) ~ sr_f * totFlow + (1 | plot)
 )
+candidate_mods <- setNames(candidate_mods, paste0("model",1:5))
 
-# # fit models
-# germ_flowN      <- lapply(candidate_mods, function(x) glmmadmb(x, family="binomial", data=germ_dat))
-# germ_flowN_sel  <- AICtab(germ_flowN,weights=T) 
-# germ_flowN_avg  <- model_avg(germ_flowN_sel, germ_flowN)
+# fit BEST model
+germ_flowN      <- lapply(candidate_mods, function(x) glmmadmb(x, family="binomial", data=germ_dat))
+germ_flowN_sel  <- AICtab(germ_flowN,weights=T)
+germ_flowN_avg  <- model_avg(germ_flowN_sel, germ_flowN)
 
 
-# function to perform bootstrapped 
-bootstrap_mod <- function(i, one_f_2){
+# function to perform bootstrapped estimates
+bootstrap_mod <- function(i, one_f_2, germ_dat, candidate_mods){
   
   # get one-female plots and test that we have 2 of them 
   one_f_data      <- subset(germ_dat, perm_id %in% one_f_2[i,] )
@@ -88,20 +106,84 @@ bootstrap_mod <- function(i, one_f_2){
   
   mod_data        <- subset(germ_dat, !(F == 1 & M == 0)) %>%
                         bind_rows( one_f_data )
-  germ_flowN      <- lapply(candidate_mods, function(x) glmmadmb(x, family="binomial", data=mod_data))
-  germ_flowN_sel  <- AICtab(germ_flowN,weights=T) 
+  germ_flowN      <- lapply(candidate_mods, function(x) glmmadmb(x, family="binomial", data=mod_data) )
+  germ_flowN_sel  <- AICtab(germ_flowN, mnames = names(germ_flowN), weights=T)  
   germ_flowN_avg  <- model_avg(germ_flowN_sel, germ_flowN)
   
   return(germ_flowN_avg)
   
 }
-# bootstrapped results
-bootstr_res_l <- lapply(1:nrow(one_f_2), bootstrap_mod, one_f_2) 
+
+boostr_par_l <- list()
+for(i in 1:nrow(one_f_2)){ 
+  
+  # get one-female plots and test that we have 2 of them 
+  one_f_data      <- subset(germ_dat, perm_id %in% one_f_2[i,] )
+  expect_equal(nrow(one_f_data), 2)
+  
+  mod_data        <- subset(germ_dat, !(F == 1 & M == 0)) %>%
+                        bind_rows( one_f_data )
+  germ_flowN      <- lapply(candidate_mods, function(x) glmmadmb(x, family="binomial", data=mod_data) )
+  germ_flowN_sel  <- AICtab(germ_flowN, mnames = names(germ_flowN), weights=T)  
+  germ_flowN_avg  <- model_avg(germ_flowN_sel, germ_flowN)
+  
+  boostr_par_l[[i]] <- germ_flowN_avg
+
+}
+
+
+# run 10 processes on the # clusters defined in "cluster"
+boostr_par_l  <- parLapply(cluster, 1:nrow(one_f_2), bootstrap_mod, one_f_2, germ_dat, candidate_mods)
+bootstr_res_l <- Map(function(x,y) tibble::add_column(x, sample = y, .before = 1),
+                     boostr_par_l, 1:length(one_f_2) )
 bootstr_res   <- Reduce(function(...) rbind(...), bootstr_res_l)
 
 boxplot(avg ~ predictor, data = bootstr_res)
 abline(h=0, lty = 2)
  
+
+
+# Graph -----------------------------------------------------------------------------
+
+# model matrix for prediction
+predic_seq<- bootstr_res %>%
+                subset( sample==1 ) %>%
+                .$predictor %>%
+                as.character
+mod_des   <- expand.grid("(Intercept)" = 1, 
+                         totFlow = seq(1,50,1),
+                         sr_f = round(seq(0,1,0.05),2)) %>%
+                mutate( 'sr_f:totFlow' = totFlow * sr_f) %>%
+                .[,predic_seq]
+
+# service functions
+range01 <- function(x)(x-min(x))/diff(range(x))
+cRamp <- function(x){
+  cols <- colorRamp(gray.colors(7,start = 0, end = 1))(range01(x))
+  apply(cols, 1, function(xt)rgb(xt[1], xt[2], xt[3], maxColorValue=255))
+}  
+
+# Sex ratio as dot color 
+par(mfrow=c(1,1),mar=c(2.6,2.5,0.2,0.1),mgp=c(1.4,0.5,0),oma=c(0,0,0,10))
+
+# viability (germination) data ---------------------------------------------------------
+plot(jitter(viabVr$totFlow,factor = 2),jitter(viabVr$germ_ratio,factor = 2),
+     pch=21,ylim=c(0,1.01),xlim=c(0,48), 
+     bg = cRamp(viabVr$sr_f), cex = 1.5,
+     xlab="Panicle density",ylab="Seed viability rate")
+
+# predicted values using 'pred' function
+plot_bootstr <- function(i){ 
+  tmp       <- subset(bootstr_res, sample == i)[,-1]
+  mod_pred  <- pred(h_des, tmp, viab, inv.logit)
+  lines(h_des$totFlow,mod_pred$viab,lwd=1,lty=1, col = "grey")
+}
+lapply(1:nrow(one_f_2), plot_bootstr)
+
+# MEAN prediction
+h_des     <- subset(mod_des,sr_f == 0.95)
+mod_pred  <- pred(h_des, germ_flowN_avg, viab, inv.logit)
+lines(h_des$totFlow,mod_pred$viab,lwd=1,lty=1)
 
 
 
