d60c7242ef5ff944377bef1c65d5c787ac781205
Compagnoni <ac22qawo@idiv.de>
2017-07-31 16:47:53 +0200

    Add test of viability in female-only plots across all densities

--- viab_females_only.R
+++ viab_females_only.R
@@ -1,6 +1,6 @@
 # Seed viability in female-only plots
 # Data: tetrazolium scoring and germination data 
-setwd("C:/Users/ac79/Downloads/Dropbox/POAR--Aldo&Tom/Response-Surface experiment/Experiment/Implementation")
+setwd("C:/Cloud/Dropbox/POAR--Aldo&Tom/Response-Surface experiment/Experiment/Implementation")
 library(lme4) ; library(bbmle) ; library(boot) ; library(testthat)
 library(dplyr) ; library(glmmADMB)
 #source("C:/Users/ac79/Documents/CODE/LLELA/model_avg.R")
@@ -22,9 +22,31 @@ viabVr  <- viabVr %>%
 # viability: female only vs. more plots with at least 1 male -----------------
 one_f   <- subset(viabVr, F == 1 ) %>%
             mutate( male_presence = 1 ) %>%
+            # male_presence == 0 is the effect of male ABSENCE, which is what we are estimating here
             mutate( male_presence = replace(male_presence, M > 0, 0) )
 
-viab_m  <- glm(cbind(germTot,germFail) ~ male_presence, 
+viab_of <- glm(cbind(germTot,germFail) ~ male_presence, 
                data = one_f, family = "binomial") 
 
-summary(viab_m)$coefficients
+summary(viab_of)$coefficients
+
+# proportions of germination
+beta_of <- coef(viab_of)
+df_one_f<- data.frame( viab_one_fem = inv.logit( sum(beta_of) ),
+                       viab_with_m  = inv.logit( beta_of[1] ) )
+
+
+# viability: ALL female-only plots -------------------------------------------
+only_f  <- subset(viabVr, F > 0 ) %>%
+              mutate( male_presence = 1 ) %>%
+              mutate( male_presence = replace(male_presence, M > 0, 0) )
+# "all females"
+viab_af <- glm(cbind(germTot,germFail) ~ male_presence, 
+               data = only_f, family = "binomial") 
+
+summary(viab_af)$coefficients
+
+# proportions of germination
+beta_af <- coef(viab_af)
+df_o_f  <- data.frame( viab_only_fem = inv.logit( sum(beta_af) ),
+                       viab_with_m  = inv.logit( beta_af[1] ) )
