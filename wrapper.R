3143a3c7b0916e2c14288743d86a7a0b5e9e9a79
Compagnoni <ac22qawo@idiv.de>
2017-09-06 11:28:20 +0200

    Add mod sel of growth rate and new tillers

--- wrapper.R
+++ wrapper.R
@@ -1,12 +1,12 @@
-setwd("C:/Users/ac79/Documents/CODE/LLELA")
+setwd("C:/CODE/LLELA")
 
 #Vital rates model selection
 source("nFlowers_mod_sel.R")
 source("fecundity_mod_sel.R")
 source("panicules_male_mod_sel.R")
-source("panicules_male_mod_sel.R")
 source("viability_mod_sel.R")
-source("growth_mod_sel.R")
+source("growth_logRatio_mod_sel.R")
+source("growth_newTillers_BH_mod_sel.R")
 
 
 # Produce figures
